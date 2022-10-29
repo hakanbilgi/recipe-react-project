0b00e75a5d146b1ebb871c81e49acb0ae9f0f11d
Hakan Bilgi <hknblg34@gmail.com>
2022-10-29 20:16:11 +0300

    recipeCard

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
-const Details = ({detailData}) => {
-
+const Details = ({ detailData }) => {
+  const { state: food } = useLocation();
+  console.log("state", food);
+  // console.log("Detailsdata", detailData);
   return <div>Details</div>;
 };
 
