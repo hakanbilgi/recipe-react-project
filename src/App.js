fe79d252f63a3021406dcc9dc36be2683ce1fb67
Hakan Bilgi <hknblg34@gmail.com>
2022-10-29 10:22:36 +0300

    components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import Header from "./components/header/Header";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Header/>
+      <Navbar/>
       
     </div>
   );
